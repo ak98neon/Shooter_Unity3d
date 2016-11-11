9bd8a7f3ac8382a61f4ee73426ebf9984a489912
ak98neon <narutoklan@Mail.ru>
2016-11-11 23:22:29 +0200

    Спавн ботов
    Спавн ботов

---
+++ Scripts/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+    private GameObject _enemy;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        if (_enemy == null)
+        {
+            _enemy = Instantiate(enemyPrefab) as GameObject;
+            _enemy.transform.position = gameObject.transform.position;
+
+            float angle = Random.Range(0, 360);
+            _enemy.transform.Rotate(0, angle, 0);
+        }
+	}
+}
